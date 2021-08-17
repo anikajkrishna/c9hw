29a17d31c8fbd98d05c3122cfbbf2705c07f60cd
anikajkrishna <86082632+anikajkrishna@users.noreply.github.com>
2021-08-16 19:31:18 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,41 @@
 var box;
 
 function setup() {
-createCanvas (400,400);
-background (51);
-box = createSprite(200,200,30,30); 
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
-function draw() {
+function draw() 
+{
+  
 
-if (keyIsDown (RIGHT_ARROW)) {
-background("red");
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red");
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+  drawSprites();
 }
 
-if (key IsDown(LEFT_ARROW)) {
-background("blue");
-}
-
-if (key IsDown (UP_ARROW)) {
-background ("yellow");
-}
-
-if (keyIsDown (DOWN_ARROW)) {
-
-background("green");
-}
-
-drawSprites();
-}
 
 
 
